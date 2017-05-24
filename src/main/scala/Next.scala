d888e51ce708b2f9344a1fb36f37f09808350619
fabio labella <fabio.labella@ncr.com>
2017-05-24 16:24:56 +0100

    First design ideas to have different wrapped types in Id (e.g. Long)

---
+++ src/main/scala/Next.scala
@@ -0,0 +1,69 @@
+object Next {
+trait Tag {
+    type Out
+  }
+  abstract class I[A <: Tag] {
+    def value: A#Out
+  }
+
+  object H extends Tag { type Out = String}
+  type H = H.type
+
+  val a = new I[H] { def value = "hello"}
+
+  val b: String = a.value
+
+  def foo(i: I[H]): String = i.value
+
+  trait Tagg[A] { type Out }
+
+  abstract class II[A] {
+    type O
+    def v: O
+    def value(implicit a: Tagg[A]): a.Out = v.asInstanceOf[a.Out]
+  }
+
+  object HH {
+    implicit def hh: Tagg[HH] {type Out = String} = new Tagg[HH] { type Out = String}
+  }
+  type HH = HH.type
+
+  val aa = new II[HH] { type O = String; def v = "hello"}
+
+  val bb : String = aa.value
+
+  def fooo(i: II[HH]): String = i.value
+
+
+
+  object TT {
+    trait Tagg[A] {
+      type Out
+      def build(a: Out): II[A] = new II[A] { def value(implicit ev: Tagg[A]): ev.Out = a.asInstanceOf[ev.Out]}
+    }
+
+    object Tagg { type Aux[I,O] = Tagg[I] { type Out = O }
+      case class Curry[A] (){
+        def apply[B](v: B)(implicit ev: Tagg.Aux[A,B]) = ev.build(v)
+      }
+      def of[A] = new Curry[A]
+    }
+
+    abstract class II[A] {
+      def value(implicit a: Tagg[A]): a.Out
+    }
+
+    object HH {
+      implicit def hh: Tagg.Aux[HH,String] = new Tagg[HH] { type Out = String}
+    }
+    type HH = HH.type
+
+    val aa = Tagg.of[HH]("hello")
+
+    val bb : String = aa.value
+
+    def fooo(i: II[HH]): String = i.value
+
+
+  }
+}
