e2b8055c6a0eecbdd38ec5bbf4c7e60162ba92bd
fabio labella <fabio.labella@ncr.com>
2017-07-03 02:06:32 +0100

    Drop default arguments in Id building

--- src/main/scala/NextId.scala
+++ src/main/scala/NextId.scala
@@ -1,4 +1,3 @@
-package eidos.id
 package next
 // sealed abstract case class to build newtypes
 // https://gist.github.com/tpolecat/a5cb0dc9adeacc93f846835ed21c92d2
@@ -12,7 +11,7 @@ sealed abstract case class Id[Tag]() {
 
   def v: V
 
-  def value(implicit ev: Build[Tag]): ev.Out = v.asInstanceOf[ev.Out]
+  def value(implicit ev: Build[Tag]): ev.V = v.asInstanceOf[ev.V]
 }
 
 object Id {
@@ -25,7 +24,7 @@ object Id {
   }
 
   class Curried[Tag]() {
-    def apply[V](v: V)(implicit b: Build.Aux[Tag, V] = Build.default[Tag], l: Label[Tag] = Label.default[Tag], ev: IsCaseObject[Tag]) = b.build(v, l)
+    def apply[V](v: V)(implicit b: Build.Aux[Tag, V], l: Label[Tag], ev: IsCaseObject[Tag]) = b.build(v, l)
   }
 
   def of[Tag] = new Curried[Tag]
@@ -37,8 +36,60 @@ object Id {
     implicit def ev[A <: Singleton with Product]: IsCaseObject[A] = null
   }
 }
+@annotation.implicitNotFound(
+    "${A} is not a valid Eidos Tag. It should extend a Label type")
+trait Label[A] {
+  def label: String
+}
 
+object Label {
+  private[next] def default[A] = new Label[A] {
+    def label = ""
+  }
 
+  private[next] sealed trait LabelDefinitionConflict
+
+  trait MakeLabel { self =>
+    // See eidos.id.Format.UUID for an explanation of this
+    // format: off
+    final def `"In Eidos, you can only extend one of MakeLabel or CustomLabel"`
+        : LabelDefinitionConflict = null
+    // format: on
+
+    implicit final def l(implicit ev: self.type <:< Product): Label[this.type] =
+      new Label[this.type] {
+        def label = self.productPrefix
+      }
+  }
+
+  trait CustomLabel {
+    // format: off
+    final def `"In Eidos, you can only extend one of MakeLabel or CustomLabel"`
+        : LabelDefinitionConflict = null
+    // format: on
+    def label: String
+
+    private def customLabel = label
+
+    implicit final def l: Label[this.type] = new Label[this.type] {
+      def label = customLabel
+    }
+  }
+
+  trait DefaultLabel {
+    // format: off
+    final def `"In Eidos, you can only extend one of MakeLabel or CustomLabel"`
+        : LabelDefinitionConflict = null
+    // format: on
+
+    implicit final def l: Label[this.type] = new Label[this.type] {
+      def label = ""
+    }
+  }
+}
+
+@annotation.implicitNotFound(
+    "${Tag} is not a valid Eidos Tag. It should extend a Build type")
 sealed trait Build[Tag] {
   type Out
   type V
@@ -49,22 +100,46 @@ object Build {
   type Aux[Tag, V_] = Build[Tag] { type V = V_ }
   type Aux2[Tag, V_, O] = Build[Tag] { type V = V_ ; type Out = O}
 
-  private[next] def default[Tag]: Build.Aux2[Tag, String, Id.Aux[Tag, String]] = new Build[Tag] {
-    type Out = Id.Aux[Tag, String]
-    type V = String
 
-    override def build(v: V, l: Label[Tag]): Out = Id.unsafeCreate(v,l)
+  trait Default {
+    private type Tag = this.type
+    implicit def b: Build.Aux2[Tag, String, Id.Aux[Tag, String]] = new Build[Tag] {
+      type Out = Id.Aux[Tag, String]
+      type V = String
+
+      override def build(v: V, l: Label[Tag]): Out = Id.unsafeCreate(v,l)
+    }
   }
+  trait Ints {
+    private type Tag = this.type
+    implicit def b: Build.Aux2[Tag, Int, Id.Aux[Tag, Int]] = new Build[Tag] {
+      type Out = Id.Aux[Tag, V]
+      type V = Int
 
+      override def build(v: V, l: Label[Tag]): Out = Id.unsafeCreate(v,l)
+    }
+  }
+}
+
+object Tags {
+  trait Default extends Build.Default with Label.DefaultLabel
 }
 
 object T {
-  case object Foo
+  case object Foo extends Tags.Default
   type Foo = Foo.type
 
   val a: Id[Foo] = Id.of[Foo]("hello")
-  //val b = a.value
-  // problem: value can't find the implicit build...would need a
-  // default argument there, which breaks parametricity
-  // def foo[A](id: Id[A]) = id.value will return String 
+  val b = a.value
+
+  def foo(id: Id[Foo]): String = {
+    id.value.toUpperCase()
+  }
+
+  case object Bar extends Build.Ints with Label.MakeLabel
+  type Bar = Bar.type
+
+  val c = Id.of[Bar](1)
+  val d = c.value
 }
+
